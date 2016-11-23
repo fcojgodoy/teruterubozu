472d3a89e8acf825e2c40f3180669d97794cc1f9
Fco.Godoy <francisco.godoy@openmailbox.org>
2016-11-23 10:03:00 +0100

    Setting up Pushy [inprogress]

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,11 @@
-<footer class="content-info">
-  <div class="container-fluid"> <?php // FIXME: no mezclar marcado en html con @extend ?>
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-    <div class="row">
-      <section class="copyright"><a href="http://demo.ghost.io">Finding The Way Home</a> © 2016</section>
-      <section class="poweredby">Proudly published with <a href="https://wordpress.org">WordPress</a></section>
+  <footer class="content-info">
+    <div class="container-fluid"> <?php // FIXME: no mezclar marcado en html con @extend ?>
+      <?php dynamic_sidebar('sidebar-footer'); ?>
+      <div class="row">
+        <section class="copyright"><a href="http://demo.ghost.io">Finding The Way Home</a> © 2016</section>
+        <section class="poweredby">Proudly published with <a href="https://wordpress.org">WordPress</a></section>
+      </div>
     </div>
-  </div>
-</footer>
+  </footer>
+
+</div> <!-- END #container for Pushy -->
--- templates/header.php
+++ templates/header.php
@@ -1,71 +1,77 @@
-<?php if (is_front_page()) : ?>
+<!-- Pushy Menu -->
+<!-- Molecule: nav-primary -->
+<nav class="nav-primary pushy pushy-right">
+  <?php
+  if (has_nav_menu('primary_navigation')) :
+    wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+  endif;
+  ?>
+</nav>
 
-<!-- Atom: front-page-banner (Add background-image style if is_front_page) -->
-<header class="banner front-page-banner" style="background-image: url('http://demo.ghost.io/content/images/2014/09/testimg-home-1.jpg')">
+<!-- Site Overlay -->
+<div class="site-overlay"></div>
 
-<?php else: ?>
+<!-- Your Content -->
 
-<!-- Atom: page-banner (Select thumbnail size according to wp_is_mobile) -->
-<header class="banner" style="background-image: url('<?php if (wp_is_mobile()) {the_post_thumbnail_url('medium');}
-      else {the_post_thumbnail_url('');}
-    ?>
-    ')">
+  <?php if (is_front_page()) : ?>
 
-<?php endif; ?>
+  <!-- Atom: front-page-banner (Add background-image style if is_front_page) -->
+  <header class="banner front-page-banner" style="background-image: url('http://demo.ghost.io/content/images/2014/09/testimg-home-1.jpg')">
 
-  <nav class="main-nav navbar">
+  <?php else: ?>
 
-    <!-- Atom: custom-logo-link -->
-    <a class="brand navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php the_custom_logo() ?></a>
-    <?php if (!has_custom_logo()) : ?>
+  <!-- Atom: page-banner (Select thumbnail size according to wp_is_mobile) -->
+  <header class="banner" style="background-image: url('<?php if (wp_is_mobile()) {the_post_thumbnail_url('medium');}
+        else {the_post_thumbnail_url('');}
+      ?>
+      ')">
+
+  <?php endif; ?>
+
+    <nav class="main-nav navbar">
+
+      <!-- Atom: custom-logo-link -->
+      <a class="brand navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php the_custom_logo() ?></a>
+      <?php if (!has_custom_logo()) : ?>
       <!-- No Custom Logo, just display the site's name -->
       <a class="brand navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <?php endif; ?>
+      <?php endif; ?>
 
-    <!-- Molecule: nav-primary -->
-    <nav class="nav-primary">
-      <!-- <a id="menu-trigger" href="#menu-example">Open Menu</a>
-      <div id="menu-example">
-        <?php
-        if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-        endif;
-        ?>
-      </div> -->
+      <!-- Molecule: menu-button -->
+      <a class="btn menu-btn">&#9776; Menu</a>
+
     </nav>
 
-  </nav>
 
-  <!-- <div class="container-fluid"> -->
 
 
     <!-- Molecule: banner-texts (Add content if is_front_page) -->
     <?php if (is_front_page()) : ?>
 
-      <div class="vertical">
-        <hgroup class="banner-texts">
+    <div class="vertical">
+      <hgroup class="banner-texts">
 
-          <!-- Atom: banner-title -->
-          <h1 class="banner-title">Finding The Way Home</h1>
+        <!-- Atom: banner-title -->
+        <h1 class="banner-title">Finding The Way Home</h1>
 
-          <!-- Atom: banner-description -->
-          <h2 class="banner-description">A beautiful narrative written with the world's most elegant publishing platform. The story begins here.</h2>
+        <!-- Atom: banner-description -->
+        <h2 class="banner-description">A beautiful narrative written with the world's most elegant publishing platform. The story begins here.</h2>
 
-        </hgroup>
+      </hgroup>
       </div>
 
-    <?php else: ?>
-    <?php endif; ?>
+      <?php else: ?>
+      <?php endif; ?>
 
-    <!-- first page of pagination -->
-    <?php if ( !is_paged() ) : ?>
+      <!-- first page of pagination -->
+      <?php if ( !is_paged() ) : ?>
 
-    <!-- Atom: scroll-down-arrow -->
-    <a class="scroll-down-arrow icon-arrow-left js-scrolltoid radial-gradient" href="#js-scrollto" data-offset="-45"><span hidden="true">Scroll Down</span></a>
+      <!-- Atom: scroll-down-arrow -->
+      <a class="scroll-down-arrow icon-arrow-left js-scrolltoid radial-gradient" href="#js-scrollto" data-offset="-45"><span hidden="true">Scroll Down</span></a>
 
-    <?php else: ?>
-    <?php endif; ?>
+      <?php else: ?>
+      <?php endif; ?>
 
-  <!-- </div> -->
+    <!-- </div> -->
 
-</header>
+  </header>
