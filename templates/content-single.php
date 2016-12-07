542cabdcdba1dfe08c06d9b71b8f8383f5806e16
Fco.Godoy <francisco.godoy@openmailbox.org>
2016-12-07 12:45:12 +0100

    create nav-post-newer

--- templates/content-single.php
+++ templates/content-single.php
@@ -78,6 +78,8 @@
   <!-- ??: Post navigation -->
   <div class="posts-navigation">
 
+
+    <!-- Molecule: nav-post-newer -->
     <?php $nextPost = get_next_post(); if($nextPost): ?>
       <?php if (has_post_thumbnail( $nextPost->ID ) ): ?>
         <?php $nextthumbnail = wp_get_attachment_image_src( get_post_thumbnail_id($nextPost->ID), '' ); $nextthumbnail = $nextthumbnail[0]; ?>
@@ -86,17 +88,15 @@
       <?php endif; ?>
 
       <!-- ?? -->
-      <div class="nav-post-newer" style="background-image:url(<?php echo $nextthumbnail; ?>);">
+      <div class="nav-post-newer">
 
-        <div class="wrap">
-          <span class="btn"><?php echo __('Read this next', 'sage') ?></span>
-          <h2>
-            <a href="<?php echo get_permalink(get_adjacent_post(false, '', false)); ?>">
-              <?php echo get_the_title( $nextPost->ID ) ?>
-            </a>
-          </h2>
-          <!-- <?php setup_postdata( $nextPost ); the_excerpt(); wp_reset_postdata(); ?> -->
-        </div>
+        <a href="<?php echo get_permalink(get_adjacent_post( false, '', false)); ?>" style="background-image:url(<?php echo $nextthumbnail; ?>);">
+          <div class="wrap">
+            <span class="btn"><?php echo __('Read this next', 'sage') ?></span>
+            <h2><?php echo get_the_title( $nextPost->ID ) ?></h2>
+            <!-- <?php setup_postdata( $nextPost ); the_excerpt(); wp_reset_postdata(); ?> -->
+          </div>
+        </a>
 
       </div>
 
@@ -108,6 +108,8 @@
 
     <?php endif; ?>
 
+
+    <!-- Molecule: nav-post-older -->
     <?php $prevPost = get_previous_post(); if($prevPost): ?>
       <?php if (has_post_thumbnail( $prevPost->ID ) ): ?>
         <?php $prevthumbnail = wp_get_attachment_image_src( get_post_thumbnail_id($prevPost->ID), '' ); $prevthumbnail = $prevthumbnail[0]; ?>
@@ -115,7 +117,6 @@
         $prevthumbnail = get_stylesheet_directory_uri() . '/images/default-featured.jpg'; ?>
       <?php endif; ?>
 
-      <!--  -->
       <div class="nav-post-older">
 
         <a href="<?php echo get_permalink(get_adjacent_post( false, '', true)); ?>" style="background-image:url(<?php echo $prevthumbnail; ?>);">
@@ -135,6 +136,7 @@
       </div> -->
 
     <?php endif; ?>
+
 
   </div>
 
