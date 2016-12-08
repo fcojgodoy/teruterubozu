b33c4aa6ee3f9b1c20a169d580e5edf9e2c776c8
Fco.Godoy <francisco.godoy@openmailbox.org>
2016-12-08 22:55:50 +0100

    desactive footer widget

--- lib/setup.php
+++ lib/setup.php
@@ -54,23 +54,23 @@ add_action('after_setup_theme', __NAMESPACE__ . '\\setup');
  * Register sidebars
  */
 function widgets_init() {
-  // register_sidebar([
-  //   'name'          => __('Primary', 'sage'),
-  //   'id'            => 'sidebar-primary',
-  //   'before_widget' => '<section class="widget %1$s %2$s">',
-  //   'after_widget'  => '</section>',
-  //   'before_title'  => '<h3>',
-  //   'after_title'   => '</h3>'
-  // ]);
-
-  register_sidebar([
-    'name'          => __('Footer', 'sage'),
-    'id'            => 'sidebar-footer',
-    'before_widget' => '<section class="widget %1$s %2$s">',
-    'after_widget'  => '</section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>'
-  ]);
+//   register_sidebar([
+//     'name'          => __('Primary', 'sage'),
+//     'id'            => 'sidebar-primary',
+//     'before_widget' => '<section class="widget %1$s %2$s">',
+//     'after_widget'  => '</section>',
+//     'before_title'  => '<h3>',
+//     'after_title'   => '</h3>'
+//   ]);
+//
+//   register_sidebar([
+//     'name'          => __('Footer', 'sage'),
+//     'id'            => 'sidebar-footer',
+//     'before_widget' => '<section class="widget %1$s %2$s">',
+//     'after_widget'  => '</section>',
+//     'before_title'  => '<h3>',
+//     'after_title'   => '</h3>'
+//   ]);
 }
 add_action('widgets_init', __NAMESPACE__ . '\\widgets_init');
 
