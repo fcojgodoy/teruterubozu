aad21dd2f8016e3ef296f3b0c1b22fb1ad4a2128
fcojgodoy <francisco.godoy@openmailbox.org>
2016-12-21 17:42:10 +0100

    created author-profile

--- templates/author-header.php
+++ templates/author-header.php
@@ -1,31 +1,42 @@
 <!-- Atom: avatar -->
-<?php echo get_avatar( get_the_author_meta('ID'), 114 ); ?>
-
-<?php use Roots\Sage\Titles; ?>
-
 <div class="page-header">
 
-  <h1><?= Titles\title(); ?></h1>
+  <div class="author-profile">
 
-  <!-- Atom: author-bio -->
-  <p class="author-bio">
-    <?php echo get_the_author_meta( 'description' ); ?>
-  </p>
+    <?php echo get_avatar( get_the_author_meta('ID'), 114 ); ?>
 
-  <!-- Atom: author-location -->
-  <?php if (get_the_author_meta('city')) : ?>
-    <em class="author-location icon-location"><?php echo get_the_author_meta( 'city' ); ?></em>
-  <?php endif ; ?>
+    <?php use Roots\Sage\Titles; ?>
 
-  <!-- Atom: author-web -->
-  <?php if (get_the_author_meta( 'url' )) : ?>
-    <em>
-      <a class="author-web icon-link" href="<?php echo get_the_author_meta( 'url' ); ?>"><?php echo get_the_author_meta( 'url' ); ?></a>
-    </em>
-  <?php endif; ?>
 
-  <?php if (get_the_author_posts()) : ?>
-    <em class="author-stats icon-stats"><?php echo get_the_author_posts() . " " . __('posts', 'sage'); ?> </em>
-  <?php endif; ?>
+    <h1 class="author-title"><?= Titles\title(); ?></h1>
 
+    <!-- Atom: author-bio -->
+    <h2 class="author-bio author-bio--lg">
+      <?php echo get_the_author_meta( 'description' ); ?>
+    </h2>
+
+    <div class="author-meta">
+
+      <!-- Atom: author-location -->
+      <?php if (get_the_author_meta('city')) : ?>
+        <em class="author-location icon-location"><?php echo get_the_author_meta( 'city' ); ?></em>
+      <?php endif ; ?>
+
+      <!-- Atom: author-web -->
+      <?php if (get_the_author_meta( 'url' )) : ?>
+        <em>
+          <a class="author-web icon-link" href="<?php echo get_the_author_meta( 'url' ); ?>"><?php echo get_the_author_meta( 'url' ); ?></a>
+        </em>
+      <?php endif; ?>
+
+      <?php if (get_the_author_posts()) : ?>
+        <em class="author-stats icon-stats"><?php echo get_the_author_posts() . " " . __('posts', 'sage'); ?> </em>
+      <?php endif; ?>
+
+    </div>
+
+  </div>
+
+  <hr class="decorative-line">
+  
 </div>
--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -2,7 +2,7 @@
 <p class="entry-meta">
 
   <!-- If is_front_page -->
-  <?php if (is_front_page()) : ?>
+  <?php if (is_front_page() || is_author()) : ?>
 
     <!-- Atom: avatar -->
     <?php echo get_avatar( get_the_author_meta('ID'), 24 ); ?>
