24c18b4536deb659acee8ee7d6f817559a18ee23
fcojgodoy <francisco.godoy@openmailbox.org>
2018-04-16 16:59:25 +0200

    remove aligner class if is home and is front-page

--- views/header/page-header.php
+++ views/header/page-header.php
@@ -52,9 +52,9 @@
 
     <header class="main-header main-header_tag aligner no-cover u-margin-bottom-large u-margin-bottom-huge@tablet">
 
-        <div class="vertical">
+        <div class="xxx-content">
             <div class="site-hgroup">
-                <h1 class="blog-title"><?php _e( 'Blog', 'teruterubozu' ) ?></h1>
+                <h1 class="blog-title"><?php esc_html_e( 'Blog', 'teruterubozu' ) ?></h1>
                 <!-- <h2 class="site-title u-h4"><?php bloginfo('name') ?></h2> -->
             </div>
         </div>
