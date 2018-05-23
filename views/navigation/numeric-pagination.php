188dad2dc82720a5578e8e84f06f47ee7a7f2288
fcojgodoy <hola@franciscogodoy.es>
2018-05-23 14:01:26 +0200

    Fighting with numeric-pagination

--- views/navigation/numeric-pagination.php
+++ views/navigation/numeric-pagination.php
@@ -16,6 +16,13 @@
 	<div class="navigation-pages o-layout__item u-1/1 u-1/3@tablet u-margin-bottom-small u-margin-bottom-none@tablet">
 
 		<?php teruterubozu_numeric_posts_pagination() ?>
+        <span><?php esc_html_e( 'Page', 'teruterubozu' ) ?></span>
+        <span><?php esc_html_e( 'of', 'teruterubozu' ) ?></span>
+        <?php the_posts_pagination( array(
+            'mid_size' => 0,
+            'prev_text' => null,
+            'next_text' => null,
+        ) ) ?>
 
 	</div>
 	<div class="nav-page-link nav-next  o-layout__item u-1/1 u-1/3@tablet u-margin-bottom-small u-margin-bottom-none@tablet">
@@ -24,3 +31,13 @@
 
 	</div>
 </nav>
+
+<div class="fixme-pagination">
+
+    <?php the_posts_pagination( array(
+        'mid_size' => 0,
+        'prev_text' => __( 'Newer Posts', 'teruterubozu' ),
+        'next_text' => __( 'Older Posts', 'teruterubozu' ),
+    ) ) ?>
+
+</div>
