39406ad3576726b74c1dbd9d314347d922828f9e
fcojgodoy <hola@franciscogodoy.es>
2018-05-25 12:53:32 +0200

    Restructuring markup of visual post navigation

--- views/navigation/posts-navigation.php
+++ views/navigation/posts-navigation.php
@@ -1,48 +1,42 @@
-<div class="posts-navigation o-pack">
+<div class="visual-post-navigation o-pack">
 
-<?php $teruterubozu_next_post = get_next_post(); if( $teruterubozu_next_post ) : ?>
+<?php if ( get_next_post() ) :
 
-    <?php
-    if ( has_post_thumbnail( $teruterubozu_next_post->ID ) ) :
-        $teruterubozu_next_thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $teruterubozu_next_post->ID ), '' );
-        $teruterubozu_next_thumbnail = $teruterubozu_next_thumbnail[ 0 ];
-    else :
-        $teruterubozu_next_thumbnail = get_stylesheet_directory_uri() . '/images/default-featured.jpg';
-    endif;
-    ?>
-
-    <div class="nav-post-newer o-pack__item">
-        <a class="nav-post-link" href="<?php echo esc_url( get_permalink( get_adjacent_post( false, '', false ) ) ) ?>" style="background-image:url(<?php echo esc_url( $teruterubozu_next_thumbnail ) ?>)">
-            <div class="wrap">
-                <span class="nav-post-button btn"><?php esc_html_e( 'Read this next', 'teruterubozu' ) ?></span>
-                <h2 class="nav-post-title"><?php echo get_the_title( $teruterubozu_next_post->ID ) ?></h2>
+    $teruterubozu_next_post = get_next_post(); ?>
+    
+    <div class="visual-post-navigation-newer o-pack__item">
+        <a class="visual-post-navigation-link" href="<?php echo esc_url( get_permalink( get_adjacent_post( false, '', false ) ) ) ?>">
+            <?php
+            if ( has_post_thumbnail( $teruterubozu_next_post->ID ) ) :
+                echo get_the_post_thumbnail( $teruterubozu_next_post->ID );
+            endif;
+            ?> 
+            <div class="wrap u-padding">
+                <span class="visual-post-navigation-button c-btn c-btn--small u-margin-bottom-small"><?php esc_html_e( 'Read this next', 'teruterubozu' ) ?></span>
+                <h2 class="visual-post-navigation-title u-margin-bottom-none"><?php echo get_the_title( $teruterubozu_next_post->ID ) ?></h2>
             </div>
         </a>
     </div>
 
 <?php endif; ?>
 
-<?php $teruterubozu_prev_post = get_previous_post(); if( $teruterubozu_prev_post ): ?>
+<?php if ( get_previous_post() ) :
 
-    <?php
-    if (has_post_thumbnail( $teruterubozu_prev_post->ID ) ):
-        $teruterubozu_prev_thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $teruterubozu_prev_post->ID ), '' );
-        $teruterubozu_prev_thumbnail = $teruterubozu_prev_thumbnail[ 0 ];
-    else :
-        $teruterubozu_prev_thumbnail = get_stylesheet_directory_uri() . '/images/default-featured.jpg';
-    endif;
-    ?>
-
-<div class="nav-post-older o-pack__item">
-
-    <a class="nav-post-link" href="<?php echo esc_url( get_permalink( get_adjacent_post( false, '', true ) ) ) ?>" style="background-image:url(<?php echo esc_url( $teruterubozu_prev_thumbnail ) ?>)">
-        <div class="wrap">
-            <span class="nav-post-button btn"><?php esc_html_e( 'You might enjoy', 'teruterubozu' ) ?></span>
-            <h2 class="nav-post-title"><?php echo get_the_title( $teruterubozu_prev_post->ID ) ?></h2>
-        </div>
-    </a>
-
-</div>
+    $teruterubozu_prev_post = get_previous_post(); ?>
+    
+    <div class="visual-post-navigation-newer o-pack__item">
+        <a class="visual-post-navigation-link" href="<?php echo esc_url( get_permalink( get_adjacent_post( false, '', true ) ) ) ?>">
+            <?php
+            if ( has_post_thumbnail( $teruterubozu_prev_post->ID ) ) :
+                echo get_the_post_thumbnail( $teruterubozu_prev_post->ID );
+            endif;
+            ?>            
+            <div class="wrap u-padding">
+                <span class="visual-post-navigation-button c-btn c-btn--small u-margin-bottom-small"><?php esc_html_e( 'You might enjoy', 'teruterubozu' ) ?></span>
+                <h2 class="visual-post-navigation-title u-margin-bottom-none"><?php echo get_the_title( $teruterubozu_prev_post->ID ) ?></h2>
+            </div>
+        </a>
+    </div>
 
 <?php endif; ?>
 
