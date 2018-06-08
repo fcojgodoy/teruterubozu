9bb745f0197abc25ce60ffd88812a0f869bf6808
Francisco J. Godoy <hola@franciscogodoy.es>
2018-06-08 13:11:17 +0200

    Code for site-branding

--- views/header/page-header.php
+++ views/header/page-header.php
@@ -63,15 +63,27 @@
 
     <nav class="navbar">
 
-        <?php if( has_custom_logo() ) : ?>
+        <div class="site-branding">
 
-            <div class="custom-logo" itemscope itemtype="http://schema.org/Organization">
-                    <?php the_custom_logo() ?>
-            </div>
+            <?php
+            the_custom_logo();
+            if ( is_front_page() && is_home() ) : ?>
+                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+            <?php else : ?>
+                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+            <?php
+            endif;
+            
+            $description = get_bloginfo( 'description', 'display' );
+            if ( $description || is_customize_preview() ) : ?>
+                <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+            <?php
+            endif; ?>
 
-        <?php endif; ?>
+        </div><!-- .site-branding -->
 
         <a class="menu-btn icon-menu" href="#"> <span class="word"><?php esc_html_e( 'Menu', 'teruterubozu' ) ?></span> </a>
+
     </nav>
 
 </header>
