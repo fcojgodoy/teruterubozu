bcc4d5fd26c4f90b107943628a079ecc6a5529de
Francisco J. Godoy <hola@franciscogodoy.es>
2018-06-08 18:55:58 +0200

    Remove site description output and styles

--- views/header/page-header.php
+++ views/header/page-header.php
@@ -70,14 +70,7 @@
                 <h1 class="site-title u-margin-bottom-tiny"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
             <?php else : ?>
                 <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-            <?php
-            endif;
-            
-            $description = get_bloginfo( 'description', 'display' );
-            if ( $description || is_customize_preview() ) : ?>
-                <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-            <?php
-            endif; ?>
+            <?php endif; ?>
 
         </div><!-- .site-branding -->
 
