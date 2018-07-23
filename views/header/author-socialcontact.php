14a43f85c94eb28df7e3b90dd5cfc5c33bfb9016
Francisco J. Godoy <hola@franciscogodoy.es>
2018-07-23 14:02:32 +0200

    User contact methods adapted to new WP User Contact Fields plugin

--- views/header/author-socialcontact.php
+++ views/header/author-socialcontact.php
@@ -14,7 +14,7 @@
     <?php if ( get_the_author_meta( 'twitter' ) ) : ?>
 
         <div class="o-pack__item">
-            <a class="icon-twitter" title="<?php esc_attr_e( 'Twitter', 'teruterubozu' ) ?>" href="https://twitter.com/<?php echo esc_html( get_the_author_meta( 'twitter' ) ) ?>" target="blank"></a>
+            <a class="icon-twitter" title="<?php esc_attr_e( 'Twitter', 'teruterubozu' ) ?>" href="https://twitter.com/<?php esc_html_e( get_the_author_meta( 'twitter' ) ) ?>" target="blank"></a>
         </div>
 
     <?php endif; ?>
@@ -22,15 +22,15 @@
     <?php if ( get_the_author_meta( 'facebook' ) ) : ?>
 
         <div class="o-pack__item">
-            <a class="icon-facebook" title="<?php esc_attr_e( 'Facebook', 'teruterubozu' ) ?>" href="https://facebook.com/profile.php?id=<?php echo esc_html( get_the_author_meta( 'facebook' ) ) ?>" target="blank"></a>
+            <a class="icon-facebook" title="<?php esc_attr_e( 'Facebook', 'teruterubozu' ) ?>" href="https://facebook.com/<?php esc_html_e( get_the_author_meta( 'facebook' ) ) ?>" target="blank"></a>
         </div>
 
     <?php endif; ?>
 
-    <?php if ( get_the_author_meta( 'googleplus' ) ) : ?>
+    <?php if ( get_the_author_meta( 'gplus' ) ) : ?>
 
         <div class="o-pack__item">
-            <a class="icon-google-plus" title="<?php esc_attr_e( 'Google +', 'teruterubozu' ) ?>" href="https://plus.google.com/<?php echo esc_html( get_the_author_meta( 'googleplus' ) ) ?>" target="blank"></a>
+            <a class="icon-google-plus" title="<?php esc_attr_e( 'Google +', 'teruterubozu' ) ?>" href="https://plus.google.com/<?php esc_html_e( get_the_author_meta( 'gplus' ) ) ?>" target="blank"></a>
         </div>
 
     <?php endif; ?>
