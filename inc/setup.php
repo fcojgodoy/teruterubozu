5eaa4714eb48df3438be7d86f422c790e2846c3e
Francisco J. Godoy <hola@franciscogodoy.es>
2018-07-25 02:30:42 +0200

    Playing with starter-content

--- inc/setup.php
+++ inc/setup.php
@@ -100,6 +100,114 @@ if ( ! function_exists( 'teruterubozu_setup' ) ) :
 			// 'flex-height' => true,
 		) );
 
+        // Define and register starter content to showcase the theme on new sites.
+    	$starter_content = array(
+    		'widgets'     => array(
+    			// Place three core-defined widgets in the sidebar area.
+    			'sidebar-1' => array(
+    				'text_business_info',
+    				'search',
+    				'text_about',
+    			),
+
+    			// Add the core-defined business info widget to the footer 1 area.
+    			'sidebar-2' => array(
+    				'text_business_info',
+    			),
+
+    			// Put two core-defined widgets in the footer 2 area.
+    			'sidebar-3' => array(
+    				'text_about',
+    				'search',
+    			),
+    		),
+
+    		// Specify the core-defined pages to create and add custom thumbnails to some of them.
+    		'posts'       => array(
+    			'home',
+    			'about'            => array(
+    				'thumbnail' => '{{image-sandwich}}',
+    			),
+    			'contact'          => array(
+    				'thumbnail' => '{{image-espresso}}',
+    			),
+    			'blog'             => array(
+    				'thumbnail' => '{{image-coffee}}',
+    			),
+    			'homepage-section' => array(
+    				'thumbnail' => '{{image-espresso}}',
+    			),
+    		),
+
+    		// Create the custom image attachments used as post thumbnails for pages.
+    		'attachments' => array(
+    			'image-espresso' => array(
+    				'post_title' => _x( 'Espresso', 'Theme starter content', 'teruterubozu' ),
+    				'file'       => 'assets/images/espresso.jpg', // URL relative to the template directory.
+    			),
+    			'image-sandwich' => array(
+    				'post_title' => _x( 'Sandwich', 'Theme starter content', 'teruterubozu' ),
+    				'file'       => 'assets/images/sandwich.jpg',
+    			),
+    			'image-coffee'   => array(
+    				'post_title' => _x( 'Coffee', 'Theme starter content', 'teruterubozu' ),
+    				'file'       => 'assets/images/coffee.jpg',
+    			),
+    		),
+
+    		// Default to a static front page and assign the front and posts pages.
+    		'options'     => array(
+    			'show_on_front'  => 'page',
+    			'page_on_front'  => '{{home}}',
+    			'page_for_posts' => '{{blog}}',
+    		),
+
+    		// Set the front page section theme mods to the IDs of the core-registered pages.
+    		'theme_mods'  => array(
+    			'panel_1' => '{{homepage-section}}',
+    			'panel_2' => '{{about}}',
+    			'panel_3' => '{{blog}}',
+    			'panel_4' => '{{contact}}',
+    		),
+
+    		// Set up nav menus for each of the two areas registered in the theme.
+    		'nav_menus'   => array(
+    			// Assign a menu to the "primary_navigation" location.
+    			'primary_navigation'    => array(
+    				'name'  => __( 'Primary navigation', 'teruterubozu' ),
+    				'items' => array(
+    					'link_home', // Note that the core "home" page is actually a link in case a static front page is not used.
+    					'page_about',
+    					'page_blog',
+    					'page_contact',
+    				),
+    			),
+
+    			// Assign a menu to the "social" location.
+    			'social' => array(
+    				'name'  => __( 'Social Links Menu', 'teruterubozu' ),
+    				'items' => array(
+    					'link_yelp',
+    					'link_facebook',
+    					'link_twitter',
+    					'link_instagram',
+    					'link_email',
+    				),
+    			),
+    		),
+    	);
+
+        /**
+    	 * Filters Teruterubozu array of starter content.
+    	 *
+    	 * @since Teruterubozu 0.2.7
+    	 *
+    	 * @param array $starter_content Array of starter content.
+    	 */
+    	$starter_content = apply_filters( 'teruterubozu_starter_content', $starter_content );
+
+    	add_theme_support( 'starter-content', $starter_content );
+
     }
 endif;
 add_action( 'after_setup_theme', 'teruterubozu_setup' );
